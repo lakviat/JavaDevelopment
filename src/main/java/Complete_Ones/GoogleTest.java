70d751a138157be0c90eb5da06391ff116e4af24
Nurlan <lakviat@yahoo.com>
2020-08-28 20:07:15 -0500

    adding some changes that pricticed

---
+++ src/main/java/Complete_Ones/GoogleTest.java
@@ -0,0 +1,20 @@
+package Complete_Ones;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class GoogleTest {
+    public static void main(String[] args) {
+
+        int [] number = {10,20,30,40};
+        String [] nukes = {};
+        List<Integer> numers = new ArrayList<>();
+
+        int firstelement = number[0];
+        int lastelement = number[number.length-1];
+
+
+
+    }
+}
