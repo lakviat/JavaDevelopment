138b49aa0c54fba5633588f38ca43a690e059ec4
lakviat <lakviat@yahoo.com>
2020-10-16 15:18:35 -0500

    adding big changes

--- src/main/java/utilities/Driver.java
+++ src/main/java/utilities/Driver.java
@@ -1,29 +1,23 @@
 package utilities;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import java.util.concurrent.TimeUnit;
-
 public class Driver {
 
-    private Driver(){
+    private Driver() {
 
     }
 
     static WebDriver driver;
-
     public static WebDriver getDriver() {
         if (driver == null) {
-
 
             driver = new ChromeDriver();
             return driver;
         }
-    driver.manage().timeouts().implicitlyWait(6, TimeUnit.SECONDS);
-        driver.manage().timeouts().pageLoadTimeout(12,TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(6, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(12, TimeUnit.SECONDS);
         return driver;
 
-
     }
-    }
+}
