b1294da6a1ed0f2968614037635cdf868028b8e6
lakviat <lakviat@yahoo.com>
2020-10-27 23:23:34 -0500

    adding snake game to master

--- src/main/java/Interface/Game.java
+++ src/main/java/Interface/Game.java
@@ -1,9 +1,34 @@
 package Interface;
 
-public class Game implements Inter.Inter2 {
+class Game implements Shape {
 
 
 
+    public void Method() {
+        System.out.println("This is from the implementing Class");
+    }
 
+    @Override
+    public void Method2() {
+        System.out.println("This is concrete method i guess");
 
+    }
 }
+
+class ImplementingClass implements Vehicle {
+    @Override
+    public void Accerlerates() {
+
+    }
+
+    @Override
+    public void Drives() {
+
+    }
+
+    @Override
+    public void Steers() {
+
+    }
+}
+
