6f543447f0ff5e6d8cf0ae2a24ac70a040267867
aleksandryavenka <evenk8548@gmail.com>
2023-04-07 18:34:43 -0500

    Commit from Alex

--- src/main/java/batch2/Alex/Alex.java
+++ src/main/java/batch2/Alex/Alex.java
@@ -3,5 +3,6 @@ package batch2.Alex;
 public class Alex {
     public static void main(String[] args) {
         System.out.println("Hi my name is Alex");
+        System.out.println("Hello this is Alex");
     }
 }
