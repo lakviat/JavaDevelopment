2022d99b32bd0b6d5a282dae1908f03b2e14606e
lakviat@yahoo.com <Nurlan259683>
2023-04-07 17:29:44 -0500

    adding from my branch

--- src/main/java/batch2/Nurlan.java
+++ src/main/java/batch2/Nurlan.java
@@ -3,5 +3,6 @@ package batch2;
 public class Nurlan {
     public static void main(String[] args) {
         System.out.println("Hello This is Nurlan");
+        System.out.println("I'm trying to add a code from my branch to master branch");
     }
 }
